e7e9727bf6df569595ee59186f7af3327f54a724
nasser <nas@gmail.com>
2023-05-12 17:35:12 +0300

    edit note view ui done

--- lib/views/edit_noteview.dart
+++ lib/views/edit_noteview.dart
@@ -8,7 +8,7 @@ class EditNoteView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return  const Scaffold(
       body: EditNoteViewBody(),
     );
   }
--- lib/widgets/edit_viewbody.dart
+++ lib/widgets/edit_viewbody.dart
@@ -2,19 +2,37 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'custom_app_bar.dart';
+import 'custom_textfield.dart';
 
 class EditNoteViewBody extends StatelessWidget {
   const EditNoteViewBody({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding:  EdgeInsets.symmetric(horizontal: 24),
-      child: Column(
-        children:  [
-          SizedBox(height: 50,),
-          CustomAppBar(title: 'Notes',icons: Icons.search),
-        ],
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24),
+        child: Column(
+          children: const [
+            SizedBox(
+              height: 50,
+            ),
+            CustomAppBar(title: ' EditNotes', icons: Icons.search),
+            SizedBox(
+              height: 50,
+            ),
+            CustomTextField(
+              hint: 'Title',
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            CustomTextField(
+              hint: 'Content',
+              maxLines: 5,
+            ),
+          ],
+        ),
       ),
     );
   }
