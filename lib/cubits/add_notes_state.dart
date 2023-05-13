3af150cbe64c9a515f9e8fa6c860a415e8db0908
nasser <nas@gmail.com>
2023-05-13 15:52:04 +0300

    register adpater (edit state)

--- lib/cubits/add_notes_state.dart
+++ lib/cubits/add_notes_state.dart
@@ -4,3 +4,12 @@ part of 'add_notes_cubit.dart';
 abstract class AddNotesState {}
 
 class AddNotesInitial extends AddNotesState {}
+
+class AddNotesLoading extends AddNotesState {}
+
+class AddNotesSucess extends AddNotesState {}
+
+class AddNotesFaliure extends AddNotesState {
+  String errorMessage;
+  AddNotesFaliure(this.errorMessage);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/adapters.dart';
+import 'package:note_app/models/note_model.dart';
 import 'package:note_app/views/notes_views.dart';
 
 import 'constant.dart';
@@ -8,6 +9,8 @@ import 'constant.dart';
 void main() async {
   await Hive.initFlutter();
   await Hive.openBox(kNotesbox);
+
+  Hive.registerAdapter(NoteModelAdapter());
   runApp(const NotesApp());
 }
 
