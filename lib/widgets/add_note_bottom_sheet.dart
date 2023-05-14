9a04b04654d5568b4def06f672654824293c69da
nasser <nas@gmail.com>
2023-05-14 14:09:22 +0300

    add add note cubit

--- lib/widgets/add_note_bottom_sheet.dart
+++ lib/widgets/add_note_bottom_sheet.dart
@@ -21,7 +21,7 @@ class AddNoteBottomSheet extends StatelessWidget {
           },
           builder: (context, state) {
            return ModalProgressHUD(
-                inAsyncCall: state is AddNotesLoading ? true : false,,
+                inAsyncCall: state is AddNotesLoading ? true : false,
                 child: addNoteFrom());
           },
         ),
